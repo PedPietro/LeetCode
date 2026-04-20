d2d9e4cdbdec5bd57456cb47c6bfd31da67ff2bf
PedPietro <166723178+PedPietro@users.noreply.github.com>
2026-04-20 20:48:57 -0300

    Desafio diário de 04/20

---
+++ 2026-04-20.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int MaxDistance(int[] colors) {
+        int distanciaMaxima = 0;
+        //int primeiraCor = 0;
+        for(int i =0; i < colors.Length; i++){
+            int primeiraCor = colors[i];
+            for(int j = i + 1; j < colors.Length; j++){
+                int segundaCor = colors[j];
+                if(primeiraCor != segundaCor){
+                    int distancia = Math.Abs(i - j);
+                    if(distancia > distanciaMaxima){
+                        distanciaMaxima = distancia;
+                    }
+                }   
+            }
+        }
+        return distanciaMaxima;
+    }
+}
+
+//about 20 minutes, without using AI help
